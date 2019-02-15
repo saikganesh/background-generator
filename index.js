bc34a79570410f4ac295913e2a61a3f92b9d0f61
Sai krishnan <saikrishnan2469@gmail.com>
2019-02-15 22:26:49 +0530

    Files have been added to github

---
+++ index.js
@@ -0,0 +1,12 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.querySelector('body')
+
+function gradientapplier(){
+	body.style.background = "linear-gradient(to right," + color1.value +"," + color2.value +")";
+	css.textContent = body.style.background+";";
+}
+color1.addEventListener("input",gradientapplier);	
+color2.addEventListener("input",gradientapplier);	
+
